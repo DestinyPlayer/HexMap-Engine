655c1ae860d512c818c30a4e3dd45d05949d5086
DestinyPlayer <thenextdecade244@gmail.com>
2021-07-23 08:53:49 +0400

    First commit, contains the basic Hex Grid and some Materials

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is basically to define all the data about a singular hex itself
+
+//Column + Row + S = 0
+//S = -(Column + Row)
+public class Hex
+{
+    public readonly int column; //Column
+    public readonly int row;    //Row
+    public readonly int s;
+
+    static readonly float HEIGHT_MATH = Mathf.Sqrt(3) / 2;
+
+    public Hex(int column, int row) //Column and Row info
+    {
+        this.column = column;
+        this.row = row;
+        this.s = -(column + row);
+    }
+
+    public Vector3 Position() //Returns the position of this hex in the world
+    {
+        float radius = 1f;
+        float width  = radius * 2;
+        float height = HEIGHT_MATH * width;
+
+        float verticalSpacing   = height;
+        float horizontalSpacing = width * 0.75f;
+        return new Vector3(
+            horizontalSpacing * this.row,
+            0,
+            verticalSpacing * (this.column + this.row / 2f)
+            );
+    }
+
+}
---
+++ Assets/Scripts/HexMap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+    void Start() // Start is called before the first frame update
+    {
+        GenerateMap();
+    }
+
+    //Let's keep all the various objects here
+    public float mapHeight, mapWidth;
+    public GameObject HexPrefab;
+    public Material[] HexMats; //0 - Water 1 - Grass 2 - Snow 3 - Desert
+
+    //Let's keep all the various objects here
+
+    private void OnDrawGizmos() //Debug position Gizmo thingy
+    {}
+
+    public void GenerateMap() //Generates the hex map
+    {
+        for (int column = 0; column < mapHeight; column++) //Vertical
+        {
+            for (int row = 0; row < mapWidth; row++)      //Horizontal
+            {
+                //Let's make a Hex
+                Hex h = new Hex(column, row);
+
+                GameObject hexObj = (GameObject)Instantiate
+                (
+                    HexPrefab,
+                    h.Position(),
+                    Quaternion.identity,
+                    this.transform
+                );
+
+                MeshRenderer mr = hexObj.GetComponentInChildren<MeshRenderer>();
+                mr.material = HexMats[Random.Range(0, HexMats.Length)];
+            }
+        }
+    }
+}
